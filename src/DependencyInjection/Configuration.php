c3686282ec29bc111c0f9cf5d50fc3a6992427f4
Franz Zieris <franz.zieris@fu-berlin.de>
2020-01-10 13:46:24 +0100

    Add support for Symfony 4.3
    TreeBuilder's root() method is deprecated since Symfony 4.3.
    The constructor for TreeBuilder was added in symfony/config 4.2, which is a
    transitive dependency of symfony/framework-bundle 4.2

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -12,8 +12,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('intriro_csv');
+        $treeBuilder = new TreeBuilder('intriro_csv');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
